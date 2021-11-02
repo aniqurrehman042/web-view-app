a870f3c60b2c9b34dd66cbd837dfb2f33c6deab9
Aniq Ur Rehman <aniqurrehman042@Aniqs-MacBook-Pro.local>
2021-11-02 13:02:48 +0500

    updated ios config

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      debugShowCheckedModeBanner: false,
       home: AnnotatedRegion<SystemUiOverlayStyle>(
         value: const SystemUiOverlayStyle(
           statusBarColor: Colors.transparent,
